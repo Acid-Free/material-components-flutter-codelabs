c81cdabd767c3fee68286b3093d5a948cb76025e
Sian Caleb Domasig <siancaleb@gmail.com>
2022-12-01 12:22:37 +0800

    Add purple color

--- mdc_100_series/lib/colors.dart
+++ mdc_100_series/lib/colors.dart
@@ -7,6 +7,8 @@ const kShrinePink400 = Color(0xFFEAA4A4);
 
 const kShrineBrown900 = Color(0xFF442B2D);
 
+const kShrinePurple = Color(0xFF5D1049);
+
 const kShrineErrorRed = Color(0xFFC5032B);
 
 const kShrineSurfaceWhite = Color(0xFFFFFBFA);
